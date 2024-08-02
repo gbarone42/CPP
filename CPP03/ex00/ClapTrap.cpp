34501b80b5e9f0e3dd77c93ebf39d2efcfbb1c28
gbarone42 <badph3@gmail.com>
2024-08-02 16:53:39 +0200

    ok

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -1,21 +1,26 @@
 #include "ClapTrap.hpp"
-//,,
+
+
+//constructor
 ClapTrap::ClapTrap(std::string name)
-    : name(name), hitPoints(10), energyPoints(10), attackDamage(0) {
+    : name(name), hitPoints(10), energyPoints(10), attackDamage(0) //set initial value
+{
     std::cout << "ClapTrap " << name << " created." << std::endl;
 }
 
+//destructor 
 ClapTrap::~ClapTrap() {
     std::cout << "ClapTrap " << name << " destroyed." << std::endl;
 }
 
+//attack method
 void ClapTrap::attack(const std::string& target)
 {
-    if (energyPoints > 0 && hitPoints > 0)//ClapTrap can’t do anything if it has no hit points or energy points left.
+    if (energyPoints > 0 && hitPoints > 0)//check if claptrap has energy points and hit points to perform an attack. //ClapTrap can’t do anything if it has no hit points or energy points left. 
     {
         std::cout << "ClapTrap " << name << " attacks " << target
                   << ", causing " << attackDamage << " points of damage!" << std::endl;
-        energyPoints--;
+        energyPoints--;//decrease energy points after attacking
     }
     else
     {
@@ -23,28 +28,29 @@ void ClapTrap::attack(const std::string& target)
     }
 }
 
+//take damage method
 void ClapTrap::takeDamage(unsigned int amount)
 {
-    if (amount >= static_cast<unsigned int>(hitPoints))
+    if (amount >= static_cast<unsigned int>(hitPoints))// // Set hit points to 0 if damage is greater than or equal to current hit points
     {
         hitPoints = 0;
         std::cout << "ClapTrap " << name << " takes " << amount << " points of damage and is now at 0 hit points!" << std::endl;
     }
     else
     {
-        hitPoints -= amount;
+        hitPoints -= amount;// subtract damage from hit points
         std::cout << "ClapTrap " << name << " takes " << amount << " points of damage and now has " << hitPoints << " hit points left!" << std::endl;
     }
 }
 
-
+//repair method
 void ClapTrap::beRepaired(unsigned int amount)
 {
-    if (energyPoints > 0 && hitPoints > 0)
+    if (energyPoints > 0 && hitPoints > 0)// check if ClapTrap can be repaired
     {
-        hitPoints += amount;
+        hitPoints += amount;//// Increase hit points by the repair amount
         std::cout << "ClapTrap " << name << " is repaired for " << amount << " hit points!" << std::endl;
-        energyPoints--;
+        energyPoints--;//// Decrease energy points after repair
     }
     else
     {
@@ -54,8 +60,7 @@ void ClapTrap::beRepaired(unsigned int amount)
 
 
 
-///getter methods
-
+////getter methods
 
 std::string ClapTrap::getName() const
 {

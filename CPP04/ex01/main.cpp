f93ae298a6f1d8de20633e7173c652164e4b6eb6
gbarone42 <badph3@gmail.com>
2024-08-07 13:24:27 +0200

    ok

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -3,27 +3,31 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-int main()
-{
+int main() {
+    {
+        Dog basic;
+        {
+            Dog tmp = basic;
+        }
+    }
+
+    std::cout << "\n--- Testing array of Animals ---\n" << std::endl;
+
     Animal* animals[4];
 
-    for (int i = 0; i < 2; ++i)
-    {
+    for (int i = 0; i < 2; ++i) {
         animals[i] = new Dog();
     }
 
-    for (int i = 2; i < 4; ++i)
-    {
+    for (int i = 2; i < 4; ++i) {
         animals[i] = new Cat();
     }
 
-    for (int i = 0; i < 4; ++i)
-    {
+    for (int i = 0; i < 4; ++i) {
         animals[i]->makeSound();
     }
 
-    for (int i = 0; i < 4; ++i)
-    {
+    for (int i = 0; i < 4; i++) {
         delete animals[i];
     }
 

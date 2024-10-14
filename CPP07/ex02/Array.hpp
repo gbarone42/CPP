437ea8ac5a1e5d222dd7df89119cc0d21f36287a
gbarone42 <badph3@gmail.com>
2024-10-14 19:14:00 +0200

    ok

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -11,26 +11,32 @@ private:
     unsigned int _size;
 
 public:
-    Array() : _array(nullptr), _size(0) {}
+    Array() : _array(NULL), _size(0) {}
 
-    Array(unsigned int n) : _size(n) {
+    Array(unsigned int n) : _size(n)
+    {
         _array = new T[n](); // Default initialize elements
     }
 
-    Array(const Array& other) {
+    Array(const Array& other)
+    {
         _size = other._size;
         _array = new T[_size];
-        for (unsigned int i = 0; i < _size; ++i) {
+        for (unsigned int i = 0; i < _size; ++i)
+        {
             _array[i] = other._array[i];
         }
     }
 
-    Array& operator=(const Array& other) {
-        if (this != &other) {
+    Array& operator=(const Array& other)
+    {
+        if (this != &other)
+        {
             delete[] _array; // Free existing resource
             _size = other._size;
             _array = new T[_size];
-            for (unsigned int i = 0; i < _size; ++i) {
+            for (unsigned int i = 0; i < _size; ++i)
+            {
                 _array[i] = other._array[i];
             }
         }
@@ -41,14 +47,17 @@ public:
         delete[] _array;
     }
 
-    T& operator[](unsigned int index) {
-        if (index >= _size) {
+    T& operator[](unsigned int index) 
+    {
+        if (index >= _size)
+        {
             throw std::out_of_range("Index out of bounds");
         }
         return _array[index];
     }
 
-    unsigned int size() const {
+    unsigned int size() const
+    {
         return _size;
     }
 };
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include "Array.hpp"
 
-int main() {
-    try {
+int main()
+{
+    try
+    {
         Array<int> intArray(5); // Create an array of 5 integers
-        for (unsigned int i = 0; i < intArray.size(); ++i) {
+        for (unsigned int i = 0; i < intArray.size(); ++i)
+        {
             intArray[i] = i * 10; // Initialize array elements
         }
 
         std::cout << "Array elements: ";
-        for (unsigned int i = 0; i < intArray.size(); ++i) {
+        for (unsigned int i = 0; i < intArray.size(); ++i)
+        {
             std::cout << intArray[i] << " ";
         }
         std::cout << std::endl;
 
         // Accessing out of bounds
         std::cout << intArray[10] << std::endl; // This should throw an exception
-    } catch (const std::exception& e) {
+    } catch (const std::exception& e)
+    {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
 

f074ae2a2bca35d4b78fda8d25d496e849e4503c
gbarone42 <badph3@gmail.com>
2024-10-21 13:35:22 +0200

    o

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,9 +1,7 @@
 #include "BitcoinExchange.hpp"
 
-int main(int argc, char** argv)
-{
-    if (argc != 2)
-    {
+int main(int argc, char** argv) {
+    if (argc != 2) {
         std::cerr << "Error: wrong number of arguments" << std::endl;
         return 1;
     }
